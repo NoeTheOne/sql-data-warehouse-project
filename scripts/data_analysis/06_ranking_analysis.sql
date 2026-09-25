e33eb1c81cd0efade8a2c25149dca8eaaddd5d6d
NoeTheOne <136508052+NoeTheOne@users.noreply.github.com>
2026-09-25 11:34:43 -0600

    Update ranking analysis SQL script for clarity and accuracy

--- scripts/data_analysis/06_ranking_analysis.sql
+++ scripts/data_analysis/06_ranking_analysis.sql
@@ -1,10 +1,17 @@
 /*
-	Exploratory Data Analysis (EDA)
-	Consists in 6 steps of exploratory actions helpng to undestand the Data on the business
+===============================================================================
+Ranking Analysis
+===============================================================================
+Purpose:
+    - To rank items (e.g., products, customers) based on performance or other metrics.
+    - To identify top performers or laggards.
+
+SQL Functions Used:
+    - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
+    - Clauses: GROUP BY, ORDER BY
+===============================================================================
 */
---=======================================================================
--- 06 >> Ranking Analysis
---=======================================================================
+
 -- Wich 5 products generate the highest revenue
 SELECT TOP 5 
 	p.product_name,
@@ -13,7 +20,7 @@ FROM gold.fact_sales s
 LEFT JOIN gold.dim_products p
 ON p.product_key = s.product_key
 GROUP BY p.product_name
-ORDER BY total_revenue DESC
+ORDER BY total_revenue DESC;
 
 SELECT * FROM (
 SELECT 
@@ -24,7 +31,7 @@ FROM gold.fact_sales s
 LEFT JOIN gold.dim_products p
 ON p.product_key = s.product_key
 GROUP BY p.product_name) t
-WHERE rank_products <= 5
+WHERE rank_products <= 5;
 
 -- What are the 5 worst-performing products in terms of sales?
 SELECT TOP 5 
@@ -34,7 +41,7 @@ FROM gold.fact_sales s
 LEFT JOIN gold.dim_products p
 ON p.product_key = s.product_key
 GROUP BY p.product_name
-ORDER BY total_revenue 
+ORDER BY total_revenue;
 
 -- Find the top 10 customers who have generated the highest revenue
 SELECT TOP 10
@@ -46,7 +53,7 @@ FROM gold.fact_sales s
 LEFT JOIN gold.dim_customers c
 ON c.customer_key = s.customer_key
 GROUP BY c.customer_key,c.first_name,c.last_name
-ORDER BY total_revenue DESC
+ORDER BY total_revenue DESC;
 
 -- Find the 3 customers with the fewest orders placed
 SELECT TOP 3
@@ -58,4 +65,4 @@ FROM gold.fact_sales s
 LEFT JOIN gold.dim_customers c
 ON c.customer_key = s.customer_key
 GROUP BY c.customer_key,c.first_name,c.last_name
-ORDER BY total_orders 
+ORDER BY total_orders;
